757bdb382e5c86f4a22488eae1e638fe3ddfcc31
Tony Mann <thephatmann@gmail.com>
2011-09-23 04:05:50 -0700

    Show song list after adding or editing song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,11 @@
 class SongsController < InheritedResources::Base
 	belongs_to :band
+
+  def create
+    create!{ collection_path }
+  end
+
+  def update
+    update!{ collection_path }
+  end
 end
