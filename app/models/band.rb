14bae5376ef44413521e9bec729b82b372c62b1d
Tony Mann <thephatmann@gmail.com>
2011-09-28 17:43:15 -0700

    Collect email

--- app/models/band.rb
+++ app/models/band.rb
@@ -3,6 +3,8 @@ class Band < ActiveRecord::Base
 	has_many :setlists
 	has_many :shows
 
-  acts_as_authentic do |c|
-  end
+  acts_as_authentic
+
+  validates_presence_of :name, :email
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110928153452) do
+ActiveRecord::Schema.define(:version => 20110929002546) do
 
   create_table "bands", :force => true do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20110928153452) do
     t.boolean  "admin"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
   create_table "setlist_items", :force => true do |t|
