f0953044fb1795ce088ee358df0b0307981957e0
Tony Mann <thephatmann@gmail.com>
2011-10-14 06:46:31 -0700

    Songs printed in columns

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,18 @@
 module ApplicationHelper
-  def title(page_title)  
+  COL_LENGTH = 25
+  NUM_COLS = 2
+
+  def title(page_title, page_stats = "")  
     content_for(:title) { page_title }  
+    content_for(:stats) { page_stats }
+  end
+
+  def column_marking(index)
+    column = NUM_COLS - (index / COL_LENGTH + 1) % NUM_COLS
+    is_column_top = index > 0 && index % COL_LENGTH == 0
+    k = 'column' + column.to_s
+    k << ' column-break' if is_column_top && column > 1
+    k << ' page-break' if is_column_top && column == 1
+    k
   end
 end
--- app/helpers/setlists_helper.rb
+++ app/helpers/setlists_helper.rb
@@ -4,4 +4,11 @@ module SetlistsHelper
     s << " (#{setlist.date.to_formatted_s(:long)})" if setlist.date
     s
   end
+
+
+  def setlist_item_class(setlist_item, index)
+    k = 'setlist_item ' + column_marking(index)
+    k << ' break' unless setlist_item.song
+    k
+  end
 end
