b5d714b24e2105310254047c71d0d545ffcee7e8
Tony Mann <thephatmann@gmail.com>
2012-01-31 17:21:45 -0800

    Failing setlist_item ordering spec

--- spec/models/setlist_item_spec.rb
+++ spec/models/setlist_item_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
 describe SetlistItem do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should preserve its order" do
+    setlist       = FactoryGirl.build(:setlist)
+    songs         = FactoryGirl.create_list(:song, 3)
+    setlist.items = songs.map{|song| SetlistItem.new(:song_id => song.id)}
+    setlist.save
+    setlist.reload
+    setlist.items.each_with_index{|item, i| item.id.should == songs[i].id}
+  end
 end
 
 # == Schema Information
